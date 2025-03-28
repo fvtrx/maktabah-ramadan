f32d2205962a14d35ffa2e8aba002ccbc3a3100b
Abdullah Fitri Che Wan <ftk2431@gmail.com>
2025-03-28 10:38:34 +0800

    update footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="py-2 sm:py-4 px-3 sm:px-6 border-t border-gray-100 text-center text-gray-400 text-xs sm:text-sm">
       <span className="font-semibold text-black/60">Maktabah Ramadan</span> •
-      Develop by{" "}
+      Built by{" "}
       <Link
         href={"https://fvtrx.com"}
         className="text-black/60 font-semibold hover:text-blue-500 transition-colors duration-300"
